5b4005639f233da1406c207b8fa2c535399b7ab7
Lebedev Vladimir <vlebedev@webim.ru>
2017-11-24 22:21:16 +0300

    change subCategory to array

--- server.js
+++ server.js
@@ -12,46 +12,68 @@ var options = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
   }
 };
-var category = 'Miscellaneous';
+var category = 'Business Formation';
 // var subCategory = 'Administration Agreement';
-var subCategory = 'liquidity agreement';
+// var subCategory = 'liquidity agreement';
+// // var subCategories = ['custodial agreements',
+// //   'asset management agreement',
+// //   'asset purchase agreement',
+// //   'asset transfer agreement',
+// //   'deposit agreements',
+// //   'liquidity agreement',
+// //   'funding agreements',
+// //   'Purchase Agreement',
+// //   'Outsourcing Agreement',
+// //   'escrow agreement',
+// //   'Letter of Intent',
+// //   'ISDA Master Agreement'];
+var subCategories = ['Articles of Association',
+  'Articles of Incorporation',
+  'Bylaws',
+  'Certificate of Incorporation',
+  'LLC Agreement',
+  'LLC Operating Agreement'];
 let fileName = '';
 
-axios.get(options.url)
-  .then((response)=> {
-    var $ = cheerio.load(response.data);
+subCategories.forEach(subCategory=> {
+  let fileName = '';
 
-    let tagForSubCategory = $('a').filter(function () {
-      return $(this).text().trim().toLowerCase()  === subCategory.toLowerCase();
-    });
-    let linkForSubCategory = tagForSubCategory[0].attribs.href;
+  axios.get(options.url)
+    .then((response)=> {
+      var $ = cheerio.load(response.data);
 
-    return axios.get(linkForSubCategory)
-  })
-  .then((response)=> {
-    $ = cheerio.load(response.data);
-    let listOfLinksToArticles = $('.row .index ul').last();
-    let linkArcticle = 'http://contracts.onecle.com' + listOfLinksToArticles.children()[0].children[0].attribs.href;
-    return axios.get(linkArcticle)
-  })
-  .then((response)=> {
-    $ = cheerio.load(response.data);
+      let tagForSubCategory = $('a').filter(function () {
+        return $(this).text().trim().toLowerCase() === subCategory.toLowerCase();
+      });
+      let linkForSubCategory = tagForSubCategory[0].attribs.href;
+      return axios.get(linkForSubCategory)
 
-    fileName = $('.col-sm-12 p')[1].children[0].data;
+    })
+    .then((response)=> {
+      $ = cheerio.load(response.data);
+      let listOfLinksToArticles = $('.row .index ul').last();
+      let linkArcticle = 'http://contracts.onecle.com' + listOfLinksToArticles.children()[0].children[0].attribs.href;
+      return axios.get(linkArcticle)
+    })
+    .then((response)=> {
+      $ = cheerio.load(response.data);
+
+      fileName = $('.col-sm-12 p')[1].children[0].data;
 //
-    let linkToFinalHtml = $('a').filter(function () {
-      return $(this).text().trim() === 'printer-friendly';
-    });
-    linkToFinalHtml = linkToFinalHtml[0].attribs.href;
-    return axios.get(linkToFinalHtml)
-  })
-  .then((response)=> {
-
-    fs.existsSync("./" + category) || fs.mkdirSync("./" + category);
-    fs.existsSync("./" + category + '/' + subCategory) || fs.mkdirSync("./" + category + '/' + subCategory);
-    fs.writeFile("./" + category + '/' + subCategory + '/' + fileName.substring(7) + '.html', response.data,()=>{});
-    // res.send(response.data);
-  })
+      let linkToFinalHtml = $('a').filter(function () {
+        return $(this).text().trim() === 'printer-friendly';
+      });
+      linkToFinalHtml = linkToFinalHtml[0].attribs.href;
+      return axios.get(linkToFinalHtml)
+    })
+    .then((response)=> {
+      fs.existsSync("./" + category) || fs.mkdirSync("./" + category);
+      fs.existsSync("./" + category + '/' + subCategory) || fs.mkdirSync("./" + category + '/' + subCategory);
+      fs.writeFile("./" + category + '/' + subCategory + '/' + fileName + '.html', response.data, ()=> {
+      });
+      // res.send(response.data);
+    })
+})
 
 
 app.get('/', function (req, res) {
@@ -87,7 +109,8 @@ app.get('/', function (req, res) {
 
       fs.existsSync("./" + category) || fs.mkdirSync("./" + category);
       fs.existsSync("./" + category + '/' + subCategory) || fs.mkdirSync("./" + category + '/' + subCategory);
-      fs.writeFile("./" + category + '/' + subCategory + '/' + fileName.substring(7) + '.html', response.data,()=>{});
+      fs.writeFile("./" + category + '/' + subCategory + '/' + fileName.substring(7) + '.html', response.data, ()=> {
+      });
       res.send(response.data);
     })
 
